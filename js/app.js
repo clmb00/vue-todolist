eebeb7958fcd1723e178e35fb264afc5d6ec9aca
clmb00 <paolo.colombo2000@gmail.com>
2022-11-04 15:38:03 +0100

    [add] print list task

--- js/app.js
+++ js/app.js
@@ -3,10 +3,36 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-
+      tasks: [
+        {
+          text: "Go shopping",
+          done: false,
+          tag: 'green'
+        },
+        {
+          text: "Go shopping",
+          done: false,
+          tag: ''
+        },
+        {
+          text: "Do homeworks",
+          done: true,
+          tag: ''
+        },
+        {
+          text: "Wash the car",
+          done: true,
+          tag: 'red'
+        },
+        {
+          text: "Eat the dog",
+          done: false,
+          tag: 'blue'
+        },
+      ]
     }
   },
   methods:{
-    
+
   }
 }).mount('#app');
